d5ae7dd2e6fa1f3fd603c3e7f6401c13e80a19e2
Sheraff <fpellet@ensc.fr>
2024-07-24 18:34:46 +0200

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { NavigationProvider } from '~/file-router/Navigation'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <NavigationProvider>
-      <App />
-    </NavigationProvider>
-  </React.StrictMode>,
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>,
 )
