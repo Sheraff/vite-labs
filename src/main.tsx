797fdbdc83f19ad3945094d949d9b1c778d37868
Sheraff <fpellet@ensc.fr>
2024-10-04 17:56:58 +0200

    fix: move service worker registration to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { NavigationProvider } from '~/file-router/Navigation'
 
+if (!window.isSecureContext) {
+  console.log("Not registering service worker because the site is not hosted on HTTPS.")
+} else {
+  navigator.serviceWorker.register('./sw.js')
+    .then(registration => {
+      console.log(`Service worker registered: scope "${registration.scope}"`)
+      registration.addEventListener("updatefound", () => window.location.reload())
+      if (registration.active && !navigator.serviceWorker.controller) {
+        window.location.reload()
+      }
+    })
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <NavigationProvider>
     <App />
